cb317ebff898e04f136b8f610d9bf7e92ad9587f
liyue43 <https://gitlab.xpaas.lenovo.com>
2025-02-19 14:26:49 +0800

    day1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <Headers></Headers>
+    <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import Headers from './components/Header'
+  import Footer from './components/Footer'
+  export default {
+    data() {
+      return {
+        key: value
+      }
+    },
+    components:{
+      Headers,
+      Footer
+    }
+  }
+</script>
 
-nav {
-  padding: 30px;
-}
+<style lang="less" scoped>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Home from '@/views/Home'
+import Login from '@/views/Login'
+import Register from '@/views/Register'
+import Search from '@/views/Search'
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/home',
+    components: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/login',
+    components: Login
+  },
+  {
+    path: '/register',
+    components:Register
+  },
+  {
+    path: '/search',
+    components:Search
+  },
 ]
 
 const router = new VueRouter({
