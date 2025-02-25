d883a1bb0f94b9eaa5fa1bb501c12a911c7734ff
liyue43 <liyue43@lenovo.com>
2025-02-25 17:41:01 +0800

    首页

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,15 +1,36 @@
 <template>
     <div>
-        我是主页面
+        <TypeNav></TypeNav>
+        <listContainer></listContainer>
+        <recommend></recommend>
+        <Rank></Rank>
+        <Like></Like>
+        <Floor></Floor>
+        <Brand></Brand>
     </div>
 </template>
 
 <script>
+import TypeNav from '@/components/TypeNav';
+import listContainer from '@/components/listContainer';
+import recommend from '@/components/recommend';
+import Rank from '@/components/Rank';
+import Like from '@/components/Like';
+import Floor from '@/components/Floor';
+import Brand from '@/components/brand';
     export default {
-        
+        components:{
+            TypeNav,
+            listContainer,
+            recommend,
+            Rank,
+            Like,
+            Floor,
+            Brand
+        }
     }
 </script>
 
-<style lang="scss" scoped>
+<style lang="less" scoped>
 
 </style>
